e5c8c85b33e9d7cdb30a762a0f292b383a6aecf1
Camara Mohamed Salim <m.salimcamara@gmail.com>
2022-04-22 22:35:35 +0200

    input dynamique terminé

--- script/search.js
+++ script/search.js
@@ -1,5 +1,19 @@
 const input = document.querySelector('.searchByIngredientsInput');
-input.addEventListener('keypress', () => {
-    input.style.width = ((input.value.length + 1) * 8) + 'px';
-    console.log(input.style.width);
+
+// allongement du champ
+input.addEventListener('keydown', (e) => {
+
+    if(e.code == "Backspace") {
+        input.style.width = ((input.value.length - 1) * 8) + 'px';
+        console.log(input.style.width + e.code);
+    } else {
+        input.style.width = ((input.value.length + 1) * 8) + 'px';
+        console.log(input.style.width + e.code);
+    }
 })
+
+// raccourcissement du champ
+// input.addEventListener('keypress', () => {
+//     input.style.width = ((input.value.length - 1) * 8) + 'px';
+//     console.log(input.style.width + 'minus');
+// })
