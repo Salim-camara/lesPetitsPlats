7c2e0ef950c5d87c0dd2c495afe0c096566e8c06
Camara Mohamed Salim <m.salimcamara@gmail.com>
2022-04-23 01:26:44 +0200

    Centrage de la flèche dans l'input

--- script/search.js
+++ script/search.js
@@ -1,8 +1,10 @@
 const input = document.querySelector('.searchByIngredientsInput');
+const buttonArrowIngredient = document.querySelector('.iconIngredientContainer');
+
+const fausseData = ['patate', 'tomate', 'artichot', 'patate douce', 'poivron', 'aubergine', 'cellerie', 'cerise', 'petit poit']
 
 // allongement du champ
 input.addEventListener('keydown', (e) => {
-
     if(e.code == "Backspace") {
         input.style.width = ((input.value.length - 1) * 8) + 'px';
         console.log(input.style.width + e.code);
@@ -10,10 +12,10 @@ input.addEventListener('keydown', (e) => {
         input.style.width = ((input.value.length + 1) * 8) + 'px';
         console.log(input.style.width + e.code);
     }
+});
+
+buttonArrowIngredient.addEventListener('click', () => {
+    
 })
 
-// raccourcissement du champ
-// input.addEventListener('keypress', () => {
-//     input.style.width = ((input.value.length - 1) * 8) + 'px';
-//     console.log(input.style.width + 'minus');
-// })
+
