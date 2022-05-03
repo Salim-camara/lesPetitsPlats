b1871d94b565c0bf34b1df4df2de9181c509681d
Camara Mohamed Salim <m.salimcamara@gmail.com>
2022-05-03 20:06:42 +0200

    Ajout des fichiers factory et data, avancement de l'input dynamique

--- script/search.js
+++ script/search.js
@@ -1,5 +1,6 @@
 const input = document.querySelector('.searchByIngredientsInput');
 const buttonArrowIngredient = document.querySelector('.iconIngredientContainer');
+const resultContainer = document.querySelector('.searchByIngredientsResult');
 
 const fausseData = ['patate', 'tomate', 'artichot', 'patate douce', 'poivron', 'aubergine', 'cellerie', 'cerise', 'petit poit']
 
@@ -7,9 +8,12 @@ const fausseData = ['patate', 'tomate', 'artichot', 'patate douce', 'poivron', '
 input.addEventListener('keydown', (e) => {
     if(e.code == "Backspace") {
         input.style.width = ((input.value.length - 1) * 8) + 'px';
+        resultContainer.style.maxWidth = ((input.value.length - 1) * 8) + 'px';
         console.log(input.style.width + e.code);
+
     } else {
         input.style.width = ((input.value.length + 1) * 8) + 'px';
+        resultContainer.style.maxWidth = ((input.value.length + 1) * 8) + 'px';
         console.log(input.style.width + e.code);
     }
 });
@@ -17,5 +21,18 @@ input.addEventListener('keydown', (e) => {
 buttonArrowIngredient.addEventListener('click', () => {
     
 })
+
+
+// récupération de la data
+
+for(const element of recipes) {
+    for(const ingredient of element.ingredients) {
+        console.log(ingredient.ingredient);
+        const newIngredient = document.createElement('p');
+        newIngredient.classList.add('ingredientContainer');
+        newIngredient.innerHTML = ingredient.ingredient;
+        resultContainer.appendChild(newIngredient);
+    }
+}
 
 
