a59f4ba0150ded25157a57d1439bb49da9050676
Camara Mohamed Salim <m.salimcamara@gmail.com>
2022-05-04 16:58:00 +0200

    Avancement des inputs

--- script/search.js
+++ script/search.js
@@ -1,5 +1,4 @@
 const input = document.querySelector('.searchByIngredientsInput');
-const buttonArrowIngredient = document.querySelector('.iconIngredientContainer');
 const resultContainer = document.querySelector('.searchByIngredientsResult');
 
 const fausseData = ['patate', 'tomate', 'artichot', 'patate douce', 'poivron', 'aubergine', 'cellerie', 'cerise', 'petit poit']
@@ -8,31 +7,50 @@ const fausseData = ['patate', 'tomate', 'artichot', 'patate douce', 'poivron', '
 input.addEventListener('keydown', (e) => {
     if(e.code == "Backspace") {
         input.style.width = ((input.value.length - 1) * 8) + 'px';
-        resultContainer.style.maxWidth = ((input.value.length - 1) * 8) + 'px';
+        resultContainer.style.width = ((input.value.length - 1) * 3) + 'ch';
         console.log(input.style.width + e.code);
-
+        
     } else {
         input.style.width = ((input.value.length + 1) * 8) + 'px';
-        resultContainer.style.maxWidth = ((input.value.length + 1) * 8) + 'px';
+        resultContainer.style.width = ((input.value.length + 1) * 3) + 'ch';
         console.log(input.style.width + e.code);
     }
 });
 
-buttonArrowIngredient.addEventListener('click', () => {
-    
-})
 
 
 // récupération de la data
+let ingredientState = false;
+const buttonArrowIngredient = document.querySelector('.iconIngredientContainer');
+const arrowIngredient = document.querySelector('.iconArrowSearch');
+const inputIngredient = document.querySelector('.searchIngredientsInput');
 
-for(const element of recipes) {
-    for(const ingredient of element.ingredients) {
-        console.log(ingredient.ingredient);
-        const newIngredient = document.createElement('p');
-        newIngredient.classList.add('ingredientContainer');
-        newIngredient.innerHTML = ingredient.ingredient;
-        resultContainer.appendChild(newIngredient);
+buttonArrowIngredient.addEventListener('click', () => {
+    if(ingredientState === false) {
+        ingredientState = true;
+        arrowIngredient.classList.add('iconArrowSearchActive');
+        resultContainer.style.width = '350px';
+
+
+        for(const element of recipes) {
+            for(const ingredient of element.ingredients) {
+                console.log(ingredient.ingredient);
+                const newIngredient = document.createElement('p');
+                newIngredient.classList.add('ingredientContainer');
+                newIngredient.innerHTML = ingredient.ingredient;
+                resultContainer.appendChild(newIngredient);
+            }
+        }
+    } else {
+        ingredientState = false;
+        arrowIngredient.classList.remove('iconArrowSearchActive');
+        resultContainer.style.width = '200px';
+        console.log('hi');
+        resultContainer.innerHTML= "";
+        
     }
-}
+})
+
+
 
 
